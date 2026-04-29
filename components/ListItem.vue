c860c7b0ab56ef1614149678b698db2543d4b9c5
Hugo Talledos <hugo.talledos05@gmail.com>
2026-04-29 14:22:51 -0500

    feat: enhance ListItem component with optional props and click event handling for improved interactivity

--- components/ListItem.vue
+++ components/ListItem.vue
@@ -15,22 +15,30 @@ export interface ItemProps {
   title: string
   description: string
   icon: IconProps
-  amount: AmountProps
-  author: string
-  date: string
+  amount?: AmountProps
+  author?: string
+  date?: string
+  clickable?: boolean
 }
 
-defineProps<ItemProps>()
+const props = defineProps<ItemProps>()
+const emit = defineEmits<{
+  (e: 'click', id: ItemProps): void
+}>()
 </script>
 <template>
-  <li class="flex items-center justify-between w-full">
+  <li
+    class="flex items-center justify-between w-full"
+    :class="{ 'cursor-pointer': clickable }"
+    @click="emit('click', props)"
+  >
     <span :class="icon.color" class="text-sm font-medium rounded-full w-12 h-12 flex items-center justify-center">{{ icon.icon }}</span>
     <div>
       <h3 class="text-lg font-bold">{{ title }}</h3>
-      <p class="text-sm text-gray-500">{{ description }}</p>
-      <p class="text-sm text-gray-500">{{ author }} - {{ date }}</p>
+      <p v-if="description" class="text-sm text-gray-500">{{ description }}</p>
+      <p v-if="author && date" class="text-sm text-gray-500">{{ author }} - {{ date }}</p>
     </div>
-    <div class="flex items-center gap-2">
+    <div v-if="amount" class="flex items-center gap-2">
       <span class="text-sm font-medium bg-gray-100 rounded-md p-2">{{ amount.short }}</span>
     </div>
   </li>
