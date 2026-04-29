0c414bfcba6dd86538a96f3e95d631e4f2ab81ac
Hugo Talledos <hugo.talledos05@gmail.com>
2026-04-29 13:00:39 -0500

    feat: add List and ListItem components for displaying items with details

---
+++ components/List.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { ItemProps } from './ListItem.vue';
+
+interface Props {
+  items: ItemProps[]
+}
+
+const props = defineProps<Props>()
+</script>
+<template>
+  <ul class="size-full bg-white rounded-md p-2 overflow-auto flex flex-col gap-4">
+    <template v-for="item in items" :key="item.id">
+      <list-item
+        :id="item.id"
+        :icon="item.icon"
+        :title="item.title"
+        :description="item.description"
+        :amount="item.amount"
+        :author="item.author"
+        :date="item.date"
+      />
+    </template>
+  </ul>
+</template>
---
+++ components/ListItem.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+interface IconProps {
+  icon: string
+  color: string
+}
+
+interface AmountProps {
+  value: string
+  short: string
+}
+
+export interface ItemProps {
+  id: string
+  title: string
+  description: string
+  icon: IconProps
+  amount: AmountProps
+  author: string
+  date: string
+}
+
+const props = defineProps<ItemProps>()
+</script>
+<template>
+  <li class="flex items-center justify-between w-full">
+    <span :class="icon.color" class="text-sm font-medium rounded-full w-12 h-12 flex items-center justify-center">{{ icon.icon }}</span>
+    <div>
+      <h3 class="text-lg font-bold">{{ title }}</h3>
+      <p class="text-sm text-gray-500">{{ description }}</p>
+      <p class="text-sm text-gray-500">{{ author }} - {{ date }}</p>
+    </div>
+    <div class="flex items-center gap-2">
+      <span class="text-sm font-medium bg-gray-100 rounded-md p-2">{{ amount.short }}</span>
+    </div>
+  </li>
+
+</template>
