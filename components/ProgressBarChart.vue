dcf8670523d52a2ed78ad2826e46c8154989bd5a
Hugo Talledos <hugo.talledos05@gmail.com>
2026-05-04 18:11:27 -0500

    refactor: update ProgressBarChart styles and props for improved visual consistency and data handling

--- components/ProgressBarChart.vue
+++ components/ProgressBarChart.vue
@@ -10,14 +10,9 @@
       >
         <div class="relative w-full h-80 flex items-end">
           <div 
-            class="absolute bottom-0 w-full border-2 border-dashed border-gray-300 rounded-3xl"
-            :style="{ height: item.targetHeight + '%' }"
-          ></div>
-
-          <div 
             :class="[
               item.color, 
-              'w-full rounded-3xl z-10 transition-all duration-500 ease-out flex flex-col items-center justify-end pb-4 shadow-sm'
+              'w-full rounded-2xl z-10 transition-all duration-500 ease-out flex flex-col items-center justify-end pb-4 shadow-sm'
             ]"
             :style="{ height: item.actualHeight + '%' }"
           >
@@ -30,6 +25,11 @@
             </div>
           </div>
 
+          <!--Linea punteada de presupuesto-->
+          <div 
+            class="absolute z-10 bottom-0 w-full border-2 border-dashed border-gray-300 rounded-2xl"
+            :style="{ height: item.targetHeight + '%' }"
+          ></div>
         </div>
       </div>
     </div>
@@ -47,19 +47,8 @@ interface Data {
   color: string
 };
 
-defineProps<{
-  data?: Data[]
-}>()
+defineProps<{ data: Data[] }>()
 
-const data: Data[] = [
-  { icon: '🍔', value: '368k', percentage: 92, actualHeight: 85, targetHeight: 95, color: 'bg-orange-100' },
-  { icon: '🎮', value: '353k', percentage: 118, actualHeight: 80, targetHeight: 65, color: 'bg-blue-100' },
-  { icon: '🛍️', value: '198k', percentage: 79, actualHeight: 45, targetHeight: 60, color: 'bg-pink-100' },
-  { icon: '💡', value: '180k', percentage: 120, actualHeight: 40, targetHeight: 30, color: 'bg-yellow-100' },
-  { icon: '✈️', value: '540k', percentage: 105, actualHeight: 90, targetHeight: 85, color: 'bg-green-100' },
-  { icon: '🚗', value: '120k', percentage: 50, actualHeight: 30, targetHeight: 60, color: 'bg-purple-100' },
-  { icon: '🏋️', value: '95k', percentage: 110, actualHeight: 75, targetHeight: 68, color: 'bg-red-100' }
-]
 </script>
 
 <style scoped>
